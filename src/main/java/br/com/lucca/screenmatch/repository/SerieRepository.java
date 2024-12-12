c9c79bf4890a045024a2e600170fcaa247dbd270
lucas <lucaston2323@gmail.com>
2024-12-12 15:23:56 -0300

    feat: montar queries JPA e JPQL

--- src/main/java/br/com/lucca/screenmatch/repository/SerieRepository.java
+++ src/main/java/br/com/lucca/screenmatch/repository/SerieRepository.java
@@ -1,8 +1,38 @@
 package br.com.lucca.screenmatch.repository;
 
+import br.com.lucca.screenmatch.model.Categoria;
+import br.com.lucca.screenmatch.model.Episodio;
 import br.com.lucca.screenmatch.model.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface SerieRepository extends JpaRepository<Serie, Long > {
+    Optional<Serie> findByTituloContainingIgnoreCase(String nomeSerie);
 
+    // GreaterThanEqual = avaliaçao deve ser maior ou igual ao valor especificado
+    List<Serie> findByAtoresContainingIgnoreCaseAndAvaliacaoGreaterThanEqual(String nomeAtor, double avaliacao);
+
+    List<Serie> findTop5ByOrderByAvaliacaoDesc();
+
+    List<Serie> findByGenero(Categoria categoria);
+
+    // opcao 1 (melhor opcao de query)
+    List<Serie> findByTotalTemporadasLessThanEqualAndAvaliacaoGreaterThanEqual(int totalTemporadas, double avaliacao);
+
+    // opcao 2 (query JPQL)
+    @Query("select s from Serie s where s.totalTemporadas <= :totalTemporadas and s.avaliacao >= :avaliacao")
+    List<Serie> seriesPorTemporadaAvaliacao(int totalTemporadas, double avaliacao);
+
+    // ILIKE = ignoreCase, % = contains
+    @Query("SELECT e FROM Serie s JOIN s.episodios e WHERE e.titulo ILIKE %:trechoEpisodio")
+    List<Episodio> episodiosPorTrecho(String trechoEpisodio);
+
+    @Query("SELECT e FROM Serie s JOIN s.episodios e WHERE s = :serie ORDER BY e.avaliacao DESC LIMIT 5")
+    List<Episodio> topEpisodiosPorSerie(Serie serie);
+
+    @Query("SELECT e FROM Serie s JOIN s.episodios e WHERE s = :serie AND YEAR(e.dataLancamento) >= :anoLancamento")
+    List<Episodio> episodiosPorSerieEAno(Serie serie, int anoLancamento);
 }
