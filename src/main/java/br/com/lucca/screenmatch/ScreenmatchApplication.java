5730f9ca6a719055de8297e29eb48945d8fe4312
lucas <lucaston2323@gmail.com>
2024-12-12 15:55:23 -0300

    feat: alterar run do projeto de cli para web

--- src/main/java/br/com/lucca/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/lucca/screenmatch/ScreenmatchApplication.java
@@ -1,25 +1,11 @@
 package br.com.lucca.screenmatch;
 
-import br.com.lucca.screenmatch.main.Main;
-import br.com.lucca.screenmatch.repository.SerieRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchApplication implements CommandLineRunner {
-
-	@Autowired
-	private SerieRepository repositorio;
-
+public class ScreenmatchApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		Main main = new Main(repositorio);
-		main.exibeMenu();
 	}
 }
