751885e9f73457cfb1d4fa2385d861da50dae588
lucas <lucaston2323@gmail.com>
2024-12-18 15:11:23 -0300

    fix: algumas correções no código

--- src/main/java/br/com/lucca/screenmatch/model/Episodio.java
+++ src/main/java/br/com/lucca/screenmatch/model/Episodio.java
@@ -1,6 +1,11 @@
 package br.com.lucca.screenmatch.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
@@ -8,34 +13,19 @@ import java.time.format.DateTimeParseException;
 @Entity
 @Table(name = "episodios")
 public class Episodio {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-    private String titulo;
+    private Long id;
     private Integer temporada;
+    private String titulo;
     private Integer numeroEpisodio;
     private Double avaliacao;
     private LocalDate dataLancamento;
     @ManyToOne
     private Serie serie;
 
-    public Episodio () {}
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public Serie getSerie() {
-        return serie;
-    }
-
-    public void setSerie(Serie serie) {
-        this.serie = serie;
-    }
+    public Episodio(){}
 
     public Episodio(Integer numeroTemporada, DadosEpisodio dadosEpisodio) {
         this.temporada = numeroTemporada;
@@ -47,19 +37,28 @@ public class Episodio {
         } catch (NumberFormatException ex) {
             this.avaliacao = 0.0;
         }
+
         try {
-             this.dataLancamento = LocalDate.parse(dadosEpisodio.dataLancamento());
-        } catch (DateTimeParseException e) {
+            this.dataLancamento = LocalDate.parse(dadosEpisodio.dataLancamento());
+        } catch (DateTimeParseException ex) {
             this.dataLancamento = null;
         }
     }
 
-    public String getTitulo() {
-        return titulo;
+    public Long getId() {
+        return id;
     }
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Serie getSerie() {
+        return serie;
+    }
+
+    public void setSerie(Serie serie) {
+        this.serie = serie;
     }
 
     public Integer getTemporada() {
@@ -68,6 +67,14 @@ public class Episodio {
 
     public void setTemporada(Integer temporada) {
         this.temporada = temporada;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
     }
 
     public Integer getNumeroEpisodio() {
@@ -96,8 +103,8 @@ public class Episodio {
 
     @Override
     public String toString() {
-        return "titulo: '" + titulo + '\'' +
-                ", temporada: " + temporada +
+        return "temporada: '" + temporada + '\'' +
+                ", titulo: " + titulo +
                 ", numeroEpisodio: " + numeroEpisodio +
                 ", avaliacao: " + avaliacao +
                 ", dataLancamento: " + dataLancamento;
